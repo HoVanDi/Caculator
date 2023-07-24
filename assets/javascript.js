3e524f12cd59e31e544e9bbd6d769b05adee095d
HoVanDi <hovandideveloper@gmail.com>
2023-07-24 14:32:09 +0700

    fix space

--- assets/javascript.js
+++ assets/javascript.js
@@ -14,11 +14,14 @@ function calculateAge() {
     dayInput > 31 ||
     monthInput < 1 ||
     monthInput > 12 ||
+    yearInput < 0 ||
+    yearInput.toString().length !== 4 ||// Kiểm tra năm phải có đúng 4 chữ số
     birthDate > today ||
-    dayInput !== birthDate.getDate() ||
+    dayInput !== birthDate.getDate() ||   //Lấy giá trị ngày trong tháng 
     monthInput - 1 !== birthDate.getMonth()
   ) {
     // Nếu ngày tháng năm không hợp lệ, không tính toán
+    alert("Vui lòng kiểm tra lại ngày, tháng, năm sinh của bạn")
     document.getElementById("years-result").innerText = "--";
     document.getElementById("months-result").innerText = "--";
     document.getElementById("days-result").innerText = "--";
@@ -26,17 +29,13 @@ function calculateAge() {
   }
 
   const ageInMilliseconds = today - birthDate;
-  const ageDate = new Date(ageInMilliseconds);
-  const years = ageDate.getUTCFullYear() - 1970;
+  const ageDate = new Date(ageInMilliseconds); //biểu thị khoảng thời gian tính được. Điều này cho phép ta thao tác với các thành phần của khoảng thời gian như năm, tháng và ngày
+  const years = ageDate.getUTCFullYear() - 1970; 
   const months = ageDate.getUTCMonth();
-  const days = ageDate.getUTCDate() - 1;
+  const days = ageDate.getUTCDate() - 1; //số ngày còn lại tính từ đầu tháng
 
   document.getElementById("years-result").innerText = years;
   document.getElementById("months-result").innerText = months;
   document.getElementById("days-result").innerText = days;
 }
 
-// Lắng nghe sự kiện input trong ô input để tự động tính toán khi nhập
-document.getElementById("day-input").addEventListener("input", calculateAge);
-document.getElementById("month-input").addEventListener("input", calculateAge);
-document.getElementById("year-input").addEventListener("input", calculateAge);
